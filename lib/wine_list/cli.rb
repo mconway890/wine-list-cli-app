0856b7de23f227175952a0f28c6cdbba28ffe494
Michele Conway <mconway890@gmail.com>
2017-11-07 17:14:58 +0000

    cli

--- lib/wine_list/cli.rb
+++ lib/wine_list/cli.rb
@@ -25,22 +25,22 @@ class WineList::CLI
       puts "Enter the number of the wine you'd like more info on, type list to see the wines again or type exit:"
       input = gets.strip.downcase
 
-      if input.to_i > 0
-        the_wine = @wines[input.to_i - 1]
+    if input.to_i > 0
+      the_wine = @wines[input.to_i - 1]
         puts ""
         puts "#{the_wine.name} - #{the_wine.rating_price}"
         puts ""
         puts "#{the_wine.description}"
       elsif input == "list"
         list_wines
-      else
-        puts "Woops! Type list or exit or enter a valid number."
+      elsif input != "exit"
+        puts "Your entry is invalid."
       end
     end
   end
 
   def goodbye
-    puts "Thank's for coming!"
+    puts "Cheers!"
   end
 
 end
